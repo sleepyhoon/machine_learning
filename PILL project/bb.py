793b9295d2736afa357e8ca4d3d26abdabf68648
sleepyhoon <101882530+sleepyhoon@users.noreply.github.com>
2023-11-27 21:20:25 +0900

    Levenshtein 예제 추가

--- PILL project/bb.py
+++ PILL project/bb.py
@@ -1,7 +1,32 @@
+'''
+대소문자 구분이 안됨
+글자수에 따라 거리의 변화가 다름.
+2글자인 경우 한글자만 달라져도 50프로가 되고
+3글자인 경우 한글자 달라지면 66프로
+4글자인 경우 한글자 달라지면 75프로... 원본과 비교했때 다른 글자수의 비율이기 때문.
+'''
+
 import Levenshtein
 
-word1 = "kitten"
-word2 = "sitting"
+def calculate_similarity(input_string, string_list):
+    similarities = []
 
-distance = Levenshtein.distance(word1, word2)
-print(f"Levenshtein distance between '{word1}' and '{word2}': {distance}")
+    for string in string_list:
+        distance = Levenshtein.distance(input_string, string)
+        similarity = 1 - (distance / max(len(input_string), len(string)))
+        similarities.append(similarity)
+
+    return similarities
+
+
+input_string = "abcd"
+
+
+string_list = ["abc", "acd", "ab", "d"]
+
+
+similarities = calculate_similarity(input_string, string_list)
+
+
+for string, similarity in zip(string_list, similarities):
+    print(f"Similarity between '{input_string}' and '{string}': {similarity:.2f}")
